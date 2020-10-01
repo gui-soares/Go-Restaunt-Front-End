011ddbfc408bc42b754bd96fca2939ca4e55a4b9
dev-guilherme-soares <guilherme.soares@zukk.com.br>
2020-09-30 23:37:37 -0300

    typings styled components

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,48 @@
+import 'styled-components';
+
+declare module 'styled-components'{
+  export interface DefaultTheme {
+    colors: {
+      red: {
+        hex: string,
+        rgba: (opacity: number) => string,
+      },
+      yellow:{
+        hex: string,
+        rgba: (opacity: number) => string,
+      },
+      title:{
+        hex: string,
+        rgba: (opacity: number) => string,
+      },
+      text:{
+        hex: string,
+        rgba: (opacity: number) => string,
+      },
+      positive: {
+        hex: string,
+        rgba: (opacity: number) => string,
+      },
+      negative: {
+        hex: string,
+        rgba: (opacity: number) => string,
+      }
+      white:{
+        hex: string,
+        rgba: (opacity: number) => string,
+      }
+      card:{
+        hex: string,
+        rgba: (opacity: number) => string,
+      },
+      card_line:{
+        hex: string,
+        rgba: (opacity: number) => string,
+      },
+      inputs:{
+        hex: string,
+        rgba: (opacity: number) => string,
+      }
+    }
+  }
+}
